1560d5da3f9ea82629ec0a7c9cab6cc291f32116
Nicolain12 <109926340+Nicolain12@users.noreply.github.com>
2023-05-13 13:04:39 -0400

    update user done, just check something with multer

--- src/controllers/usersApiController.js
+++ src/controllers/usersApiController.js
@@ -163,15 +163,16 @@ module.exports = {
             }
         }
         try {
+            console.log(req.file);
             let newData = {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
+                firstName: req.body.name,
+                lastName: req.body.surname,
                 email: req.body.email,
                 image: req.file ? req.file.filename : 'default.jpg',
-                admin: req.body.admin
             }
-            const edited = await Users.update(newData, { where: { id: req.params.id } })
-            response.data = newData
+            await Users.update(newData, { where: { id: req.params.id } })
+            const user = await Users.findByPk(req.params.id)
+            response.data = user
             res.json(response)
 
         } catch (e) {
@@ -212,10 +213,10 @@ module.exports = {
                 status: 200
             }
         }
-        if(req.token){
+        if (req.token) {
             response.data = req.token.finded
             return res.json(response)
-        } else{
+        } else {
             response.info.status = 400
             response.info.msg = 'User not found'
             return res.json(response)
--- src/routes/usersApi.js
+++ src/routes/usersApi.js
@@ -3,16 +3,15 @@ const router = express.Router()
 const usersAPI = require('../controllers/usersApiController')
 const path = require('path');
 const authorizationToken = require('../middlewares/authentication')
-
 //MULTER
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        let folder = path.join(__dirname, '../../public/images/products')
+        let folder = path.join(__dirname, '../../public/images/users')
         cb(null, folder)
     },
     filename: function (req, file, cb) {
-        let imageName ='products-' +  Date.now() + path.extname(file.originalname)
+        let imageName ='user-' +  Date.now() + path.extname(file.originalname)
         cb(null, imageName)
     }
 })
@@ -31,7 +30,7 @@ router.post('/register', usersAPI.register)
 router.post('/loggin', usersAPI.loggin)
 
 // Update user
-router.put('/update/:id', usersAPI.update)
+router.put('/update/:id',authorizationToken, upload.single('fileEdit'), usersAPI.update)
 
 // Delete user
 router.delete('/delete/:id', usersAPI.delete)
